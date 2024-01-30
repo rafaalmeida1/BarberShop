145f5ca070a126601aa6d03efc06e352c0c22a2f
rafaalmeida1 <rafaprof312@gmail.com>
2024-01-30 10:45:53 -0300

    feat: welcome

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,20 @@
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
     return (
         <div>
             <Header />
+
+            <div className="pt-5 px-5">
+                <h2 className="text-xl font-bold">Olá, Rafa!</h2>
+                <p className="text-sm capitalize">
+                    {format(new Date(), "EEEE',' d 'de' MMMM", {
+                        locale: ptBR,
+                    })}
+                </p>
+            </div>
         </div>
     );
 }
