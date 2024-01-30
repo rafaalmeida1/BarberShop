6a30ddddd1bc35fe16de01d0e62afb0c11eff446
rafaalmeida1 <rafaprof312@gmail.com>
2024-01-30 10:39:51 -0300

    feat: header

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,9 @@
+import Header from "../_components/header";
+
 export default function Home() {
     return (
         <div>
-            <h1>Hello</h1>
+            <Header />
         </div>
     );
 }
---
+++ app/_components/header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Card, CardContent } from "./ui/card";
+import { MenuIcon } from "lucide-react";
+
+export default function Header() {
+    return (
+        <Card>
+            <CardContent className="flex flex-row items-center justify-between px-5 py-8">
+                <Image src="/logo.png" alt="Logo" width={120} height={22} />
+                <Button size="icon" variant="outline" className="w-8 h-8">
+                    <MenuIcon size={16} />
+                </Button>
+            </CardContent>
+        </Card>
+    );
+}
