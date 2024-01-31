217ce0f74b75fa61d538ad582fa62cda3b6f993f
rafaalmeida1 <rafaprof312@gmail.com>
2024-01-31 11:26:08 -0300

    feat:add services item component

---
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Service } from "@prisma/client";
+import Image from "next/image";
+
+interface IServiteItem {
+    service: Service;
+}
+
+export default function ServiceItem({ service }: IServiteItem) {
+    return (
+        <Card className="hover:border-zinc-400 border transition-all duration-150">
+            <CardContent className="p-3">
+                <div className="flex gap-4 items-center">
+                    <div className="min-h-[110px] max-h-[110px] min-w-[110px] max-w-[110px] relative">
+                        <Image
+                            src={service.imageUrl}
+                            fill
+                            alt={service.name}
+                            className="rounded-lg object-contain"
+                        />
+                    </div>
+
+                    <div className="flex flex-col w-full">
+                        <h2 className="font-bold">{service.name}</h2>
+                        <p className="text-gray-400 text-sm">
+                            {service.description}
+                        </p>
+
+                        <div className="flex items-center justify-between mt-3">
+                            <p className="text-violet-500 text-sm font-bold">
+                                {Intl.NumberFormat("pt-BR", {
+                                    style: "currency",
+                                    currency: "BRL",
+                                }).format(Number(service.price))}
+                            </p>
+                            <Button variant="secondary">Reservar</Button>
+                        </div>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    );
+}
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/app/_lib/prisma";
 import BarbershopInfo from "./_components/BarbershopInfo";
+import ServiceItem from "./_components/service-item";
 
 interface IBarbershopDetails {
     params: {
@@ -19,6 +20,9 @@ export default async function BarbershopDetailsPage({
         where: {
             id: params.id,
         },
+        include: {
+            services: true,
+        },
     });
 
     if (!barbershop) {
@@ -28,6 +32,12 @@ export default async function BarbershopDetailsPage({
     return (
         <div>
             <BarbershopInfo barbershop={barbershop} />
+
+            <div className="flex flex-col gap-3 pt-6 px-5">
+                {barbershop.services.map((service) => (
+                    <ServiceItem key={service.id} service={service} />
+                ))}
+            </div>
         </div>
     );
 }
