aa1cf918cb874407febbd51a9cb5577b6c0d3cc8
rafaalmeida1 <rafaprof312@gmail.com>
2024-02-01 11:19:05 -0300

    feat: auth provider

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,21 +4,15 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import { MenuIcon } from "lucide-react";
-import { signIn } from "next-auth/react";
 
 export default function Header() {
-    const handleLoginClick = async () => {
-        await signIn();
-    };
-
     return (
         <Card>
             <CardContent className="flex flex-row items-center justify-between px-5 py-8">
                 <Image src="/logo.png" alt="Logo" width={120} height={22} />
-                {/* <Button size="icon" variant="outline" className="w-8 h-8">
+                <Button size="icon" variant="outline" className="w-8 h-8">
                     <MenuIcon size={16} />
-                </Button> */}
-                <Button onClick={handleLoginClick}>Login</Button>
+                </Button>
             </CardContent>
         </Card>
     );
